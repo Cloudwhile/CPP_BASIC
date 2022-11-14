e7039292642306329b2a38e243bc708250e948ed
Cloudwhile <86512509+Cloudwhile@users.noreply.github.com>
2022-11-14 19:04:35 +0800

    Update Hello world.cpp

---
+++ resources/Hello world/Hello world.cpp
@@ -0,0 +1,15 @@
+//Hello world.cpp
+
+#include <iostream> //C++ Basic library
+
+int main(){
+  
+  std::cout<<"Hello world"<<std::endl; //output function
+  
+  system("pause"); //block program acceptance
+  /* int a;
+     std::cin>>a;
+  */
+  
+  return 0; //return value
+}
